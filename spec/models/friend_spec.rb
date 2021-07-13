1075adb8fe135e2cf2c53860a0a2c98ee962b9cc
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-13 15:42:10 -0600

    added test for gift model

--- spec/models/friend_spec.rb
+++ spec/models/friend_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Friend do
   describe 'relationhips' do
-    it { should belong_to(:friend) }
+    it { should belong_to(:user) }
     it { should have_many(:friend_gifts) }
     it { should have_many(:gifts).through(:friend_gifts) }
   end
