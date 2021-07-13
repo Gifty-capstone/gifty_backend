c79d94453540bfd718093aa4f6596836dbfecdee
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-13 15:42:10 -0600

    added test for gift model

--- spec/models/gift_spec.rb
+++ spec/models/gift_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Gift, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationhips' do
+    it { should have_many(:friend_gifts) }
+    it { should have_many(:friends).through(:friend_gifts) }
+  end
 end
