dcc6ca9ca4476bbf112996c1ef038e17e771cab3
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-14 08:54:13 -0600

    created need gift method and tests

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -5,7 +5,13 @@ class Friend < ApplicationRecord
   has_many :friend_gifts
   has_many :gifts, through: :friend_gifts
 
-  def self.need_gift
-    binding.pry
+  def need_gift
+    this_years_bday = Date.new(Time.now.year, birthday.month, birthday.day)
+    need = gifts.where(updated_at: this_years_bday.prev_year.. this_years_bday)
+     if need.count == 0
+      true
+     else
+      false
+     end
   end
 end
