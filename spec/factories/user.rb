05c16a2573a84af0ea3a240151b89ca9bdda3b82
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-13 19:55:57 -0600

    created friends controller and routes for cruding friends

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :friend do
     name { Faker::Name.first_name }
+    email { Faker::Internet.email }
     birthday { Faker::Date.birthday(min_age: 18, max_age: 65) }
     icon { 1 }
-    association :user
   end
 end
