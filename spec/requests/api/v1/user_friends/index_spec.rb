de905bf3cc999f64741fffc2472382dbf1cc7923
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-13 20:01:22 -0600

    added factory fakers

--- spec/requests/api/v1/user_friends/index_spec.rb
+++ spec/requests/api/v1/user_friends/index_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 describe 'User Friends' do
   describe 'User Friends All' do 
     describe  'Happy path' do 
-      it 'sends all  of a users friends'
+      it 'sends all  of a users friends' do
+        
+      end
     end
   end
 end
