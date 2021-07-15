9e927240a0c666cd9319bcdc31e89a3849cb2b50
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-14 23:37:53 -0600

    rubocop errors fixed

--- app/serializers/friend_serializer.rb
+++ app/serializers/friend_serializer.rb
@@ -2,7 +2,7 @@ class FriendSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :birthday, :id, :memo
   has_many :gifts
-  
+
   attribute :need_gift do |object|
     object.need_gift
   end
