8804230f46ac1add6030b9cd91347e8061bb72a7
Brisa Garcia <brisagarciaglz@gmail.com>
2021-07-18 12:33:12 -0600

    Add limit 5 products

--- app/facades/etsy_facade.rb
+++ app/facades/etsy_facade.rb
@@ -1,6 +1,6 @@
 class EtsyFacade
   def self.product(keyword)
-    results = EtsyService.get_product(keyword)[:results]
+    results = EtsyService.get_product(keyword)[:results][0..4]
     results.map do |product|
       Product.new(product, keyword)
     end
