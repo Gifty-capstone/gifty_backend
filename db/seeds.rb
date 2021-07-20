c9a209fe1a2c56c8d551d6bebe55e225da78a2ac
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-20 16:49:38 -0600

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,19 @@ Gift.destroy_all
 Friend.destroy_all
 User.destroy_all
 
-user = User.create!( name: 'Dione Travis', email: 'user@email.com', birthday: '2020-11-03')
+user = User.create!( name: 'Travis', email: 'user@email.com', birthday: '2020-11-03')
 
 
-friend_1 = Friend.create!( name: 'Kendall', birthday: '2020-12-11', user_id: user.id, memo: " Enjoys things")
-friend_2 = Friend.create!( name: 'Melanie', birthday: '2020-11-09', user_id: user.id, memo: " Enjoys stuff")
-friend_3 = Friend.create!( name: 'Rachel', birthday: '2020-07-08', user_id: user.id, memo: " Enjoys all sorts of things and stuff")
+friend_1 = Friend.create!( name: 'Kendall', birthday: '2021-12-11', user_id: user.id, memo: "Enjoys things")
+friend_2 = Friend.create!( name: 'Melanie', birthday: '2021-11-09', user_id: user.id, memo: "Enjoys stuff")
+friend_3 = Friend.create!( name: 'Rachel', birthday: '2021-08-15', user_id: user.id, memo: "Enjoys all sorts of things and stuff")
+friend_4 = Friend.create!( name: 'Brisa', birthday: '2021-08-10', user_id: user.id, memo: "Likes dancing")
 
-gift_1 = Gift.create!( name: 'Lego', description: 'building blocks', status: 1, friend_id: friend_1.id )
-gift_2 = Gift.create!( name: 'Monopoly', description: 'fast-dealing property trading board game', status: 0, friend_id: friend_1.id )
-gift_3 = Gift.create!( name: 'Barbie', description: 'popular doll', status: 1, friend_id: friend_2.id )
-gift_4 = Gift.create!( name: 'My Little Pony', description: 'cute ponies with tattoo', status: 1, friend_id: friend_2.id )
-gift_5 = Gift.create!( name: 'Transformers', description: 'Car monsters', status: 0, friend_id: friend_3.id)
+gift_1 = Gift.create!( name: 'Lego', description: 'building blocks', status: 1, friend_id: friend_1.id ) #won't need gift
+gift_2 = Gift.create!( name: 'Blender', description: 'Vitamix', status: 0, friend_id: friend_1.id )
+gift_3 = Gift.create!( name: 'Barbie', description: 'popular doll', status: 1, friend_id: friend_2.id ) # wont need gift
+gift_4 = Gift.create!( name: 'Row Machine', description: 'Water Rower', status: 1, friend_id: friend_2.id )
+gift_5 = Gift.create!( name: 'Transformers', description: 'Car monsters', status: 0, friend_id: friend_3.id) #Need a gift
+gift_6 = Gift.create!( name: 'Book', description: 'Historical Fiction', status: 0, friend_id: friend_3.id)
+gift_5 = Gift.create!( name: 'Candles', description: 'No wiccan candles please', status: 1, friend_id: friend_4.id) #Wont Need a gift
+gift_5 = Gift.create!( name: 'Transformers', description: 'Car monsters', status: 0, friend_id: friend_4.id)
