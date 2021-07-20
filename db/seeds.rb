61b306a160dedc7719cf0bc64bad39e2701e4835
Brisa Garcia <brisagarciaglz@gmail.com>
2021-07-20 15:51:59 -0600

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ friend_2 = Friend.create!( name: 'Melanie', birthday: '2020-11-09', user_id: use
 friend_3 = Friend.create!( name: 'Rachel', birthday: '2020-07-08', user_id: user.id, memo: " Enjoys all sorts of things and stuff")
 
 gift_1 = Gift.create!( name: 'Lego', description: 'building blocks', status: 1, friend_id: friend_1.id )
-gift_2 = Gift.create!( name: 'Monopoly', description: 'capitalisnm 101', status: 0, friend_id: friend_1.id )
+gift_2 = Gift.create!( name: 'Monopoly', description: 'fast-dealing property trading board game', status: 0, friend_id: friend_1.id )
 gift_3 = Gift.create!( name: 'Barbie', description: 'popular doll', status: 1, friend_id: friend_2.id )
 gift_4 = Gift.create!( name: 'My Little Pony', description: 'cute ponies with tattoo', status: 1, friend_id: friend_2.id )
 gift_5 = Gift.create!( name: 'Transformers', description: 'Car monsters', status: 0, friend_id: friend_3.id)
